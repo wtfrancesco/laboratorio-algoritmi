05e266e018e767f309b18e770bfc50d59acba3c5
wtfrancesco <francescodicristinzi@icloud.com>
2023-10-13 00:14:39 +0200

    numeroconsonanti

--- Lab1/numeroconsonanti/numeroconsonanti.go
+++ Lab1/numeroconsonanti/numeroconsonanti.go
@@ -2,8 +2,34 @@ package main
 
 import (
 		"fmt"
-)
+		"bufio"
+		"os"
+		"strings"
 
+)
+func numeroConsonanti(stringhe []string) {
+
+	for _,stringa := range stringhe{
+		count := 0 
+		for _, runa := range stringa{
+			if(strings.ContainsRune("bcdfghlmnpqrstvz", runa)){
+				count++
+			}
+		}
+		fmt.Println(stringa, "contiene ", count, "consonanti")
+	}
+}
 func main (){
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var stringhe []string
+
+	for scanner.Scan(){
+		stringhe = append(stringhe, scanner.Text())
+	}
+
+	numeroConsonanti(stringhe)
+
 
 }
