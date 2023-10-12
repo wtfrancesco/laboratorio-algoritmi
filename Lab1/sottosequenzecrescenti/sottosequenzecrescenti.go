dd4c329ef5b6e217409225cc414221fd93d47903
wtfrancesco <francescodicristinzi@icloud.com>
2023-10-13 00:40:27 +0200

    sottosequenzecrescenti

---
+++ Lab1/sottosequenzecrescenti/sottosequenzecrescenti.go
@@ -0,0 +1,33 @@
+//diEOF.Adesempio,suinput1 2 13 0 7 8 9 -1 0 2ilprogrammastampalesomme16,24 e 1.
+//non mi serve salvare 
+package main
+
+import (
+		"fmt"
+)
+
+func main(){
+	current := 0;
+	prev:= 0
+	fmt.Scan(&prev)
+	somme := prev
+	
+	for{
+		_, err := fmt.Scan(&current)
+	
+		if(err != nil){
+			if(somme != 0){
+				fmt.Println(somme)
+			}
+			break
+		}
+
+		if(current > prev){
+			somme += current
+		}else{
+			fmt.Println(somme)
+			somme = current
+		}
+		prev = current	
+	}
+}
