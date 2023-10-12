badd4fe04f6e9c502b7d2ab6b3d213b4e8266d02
wtfrancesco <francescodicristinzi@icloud.com>
2023-10-12 13:04:15 +0200

    elettricita

--- Lab1/elettricita/elettricita.go
+++ Lab1/elettricita/elettricita.go
@@ -4,9 +4,30 @@ import (
 	"fmt"
 )
 
+/*
+4 2 1 
+
+*/
+
 
 func main (){
+	var consumiEnergetici []int
+	consumo := 0
+	fmt.Println("9999 per terminare")
+	for {
+		fmt.Scan(&consumo)
+		if(consumo == 9999){
+			break
+		}
+		consumiEnergetici = append(consumiEnergetici, consumo)
+	}
 
+	for i := 1; i < len(consumiEnergetici)-1 ; i++{
+		if(consumiEnergetici[i-1] > consumiEnergetici[i] && consumiEnergetici[i]<consumiEnergetici[i+1]){
+			fmt.Print(i+1, " ")
+		}
+	}
 
+	fmt.Println()
 
 }
